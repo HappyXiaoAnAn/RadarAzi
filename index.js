d912597a284bae374bfe0ea4b82163fd14a00d4f
Happy小安安 <andy85728@gmail.com>
2022-07-07 16:04:19 +0800

    version2
    Zoom & drag

--- index.js
+++ index.js
@@ -1,12 +1,94 @@
+var canvas;
+var ctx;
+var widthCanvas;
+var heightCanvas;
+// View parameters
+var xleftView = 0;
+var ytopView = 0;
+var widthViewOriginal = 400;           //actual width and height of zoomed and panned display
+var heightViewOriginal = 400;
+var widthView = widthViewOriginal;           //actual width and height of zoomed and panned display
+var heightView = heightViewOriginal;
 
-const canvas = document.querySelector('#canvas');
-const ctx = canvas.getContext('2d');
+window.addEventListener("load",setup,false);
+
+function setup() {
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+
+    widthCanvas = canvas.width;
+    heightCanvas = canvas.height;
+
+    // canvas.addEventListener("dblclick", handleDblClick, false);  // dblclick to zoom in at point, shift dblclick to zoom out.
+    canvas.addEventListener("mousedown", handleMouseDown, false); // click and hold to pan
+    canvas.addEventListener("mousemove", handleMouseMove, false);
+    canvas.addEventListener("mouseup", handleMouseUp, false);
+    canvas.addEventListener("mousewheel", handleMouseWheel, false); // mousewheel duplicates dblclick function
+    canvas.addEventListener("DOMMouseScroll", handleMouseWheel, false); // for Firefox
+
+    draw();
+}
+var mouseDown = false;
+
+function handleMouseDown(event) {
+    mouseDown = true;
+}
+
+function handleMouseUp(event) {
+    mouseDown = false;
+}
+
+var lastX = 0;
+var lastY = 0;
+function handleMouseMove(event) {
+
+    var X = event.clientX - this.offsetLeft - this.clientLeft + this.scrollLeft;
+    var Y = event.clientY - this.offsetTop - this.clientTop + this.scrollTop;
+
+    if (mouseDown) {
+        var dx = (X - lastX) / widthCanvas * widthView;
+        var dy = (Y - lastY)/ heightCanvas * heightView;
+    xleftView -= dx;
+    ytopView -= dy;
+    xleftView = Math.max(xleftView, 0);
+    ytopView = Math.max(ytopView, 0);
+    xleftView = Math.min(xleftView, 400-widthView);
+    ytopView = Math.min(ytopView, 400-heightView);
+    }
+    lastX = X;
+    lastY = Y;
+
+    draw();
+}
+
+function handleMouseWheel(event) {
+    var x = widthView/2 + xleftView;  // View coordinates
+    var y = heightView/2 + ytopView;
+
+    var scale = (event.wheelDelta < 0 || event.detail > 0) ? 1.1 : 0.9;
+    widthView *= scale;
+    heightView *= scale;
+
+    if (widthView > widthViewOriginal || heightView > heightViewOriginal) {
+    widthView = widthViewOriginal;
+    heightView = heightViewOriginal;
+    x = widthView/2;
+    y = heightView/2;
+    }
+
+    // scale about center of view, rather than mouse position. This is different than dblclick behavior.
+    xleftView = x - widthView/2;
+    ytopView = y - heightView/2;
+    xleftView = Math.max(xleftView, 0);
+    ytopView = Math.max(ytopView, 0);
+    xleftView = Math.min(xleftView, 400-widthView);
+    ytopView = Math.min(ytopView, 400-heightView);
+
+    draw();
+}
 
 img = new Image;
 img.src = "https://www.cwb.gov.tw/Data/radar/CV1_TW_1000_forPreview.png"
-img.onload = function(){
-    draw();
-}
 
 function draw() {
 
@@ -14,34 +96,15 @@ function draw() {
         return;
     }
     ctx.clearRect(0,0,400,400);
+    
+    ctx.setTransform(1,0,0,1,0,0);
+    ctx.scale(widthCanvas/widthView, heightCanvas/heightView);
+    ctx.translate(-xleftView,-ytopView);
 
     ctx.drawImage(img, 0, 0, 400, 400);
-
-    // ctx.drawImage(img, 0, 0, 400, 400);
     // set line stroke and line width
-    ctx.strokeStyle = 'black';
-    ctx.lineWidth = 1;
-
-    // draw a out-line
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(400, 0);
-    ctx.stroke();
-    // ctx.beginPath();
-    ctx.moveTo(400, 0);
-    ctx.lineTo(400, 400);
-    ctx.stroke();
-    // ctx.beginPath();
-    ctx.moveTo(400, 400);
-    ctx.lineTo(0, 400);
-    ctx.stroke();
-    // ctx.beginPath();
-    ctx.moveTo(0, 400);
-    ctx.lineTo(0, 0);
-    ctx.stroke();
-
-
     ctx.strokeStyle = 'red';
+    ctx.lineWidth = 1;
     x0 = 118
     y0 = 20.5
     dx = 400/6
@@ -55,7 +118,7 @@ function draw() {
     // point
     ctx.fillStyle = 'red'
     ctx.beginPath();
-    ctx.arc(px, py, 5, 0, 2*Math.PI);
+    ctx.arc(px, py, 3, 0, 2*Math.PI);
     ctx.closePath();
     ctx.fill();
     // circle
@@ -64,8 +127,22 @@ function draw() {
     ctx.closePath();
     ctx.stroke();
 
-    // line
+    // azi
     draw_azi();
+
+    // draw a out-line
+    ctx.strokeStyle = 'black';
+    ctx.setTransform(1,0,0,1,0,0);
+    ctx.scale(1, 1);
+    ctx.translate(0,0);
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(400, 0);
+    ctx.lineTo(400, 400);
+    ctx.lineTo(0, 400);
+    ctx.lineTo(0, 0);
+    ctx.stroke();
+
     ctx.restore();
 
 }
